fea6da97c112c9294ce1b5a6584bfec3e8f0d619
saad <saadahmedsiddiqui.01@gmail.com>
2018-10-22 18:40:10 -0700

    Working program

---
+++ Links.java
@@ -0,0 +1,24 @@
+
+public class Links {
+	
+	private int id;
+	private String name;
+	
+	public Links(int i, String n){
+		this.id=i;
+		this.name=n;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
